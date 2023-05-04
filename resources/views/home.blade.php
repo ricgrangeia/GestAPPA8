5fd1c4ea9ace9bf7eb0383343a4ea386e7e30c6d
ricardograngeia <ricardograngeia@gmail.com>
2023-05-04 23:03:56 +0100

    Teste

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    <p class="mb-0">You are logged in!</p>
+                    <p class="mb-0">You are logged 2 in!</p>
                 </div>
             </div>
         </div>
